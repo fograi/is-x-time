519638711953b1fa2b12adda6805a2a6439f424e
fograi <95660527+fograi@users.noreply.github.com>
2022-03-08 22:41:47 -0500

    Makes nowHour a function

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-export const nowHour = new Date(Date.now()).getHours();
+export const nowHour = () => new Date(Date.now()).getHours();
 
-export const isDayTime = () => nowHour > 5 && nowHour < 18;
+export const isDayTime = () => nowHour() > 5 && nowHour() < 18;
 
 export const isNightTime = () => !isDayTime();
 
-export const isNight = () => nowHour >= 0 && nowHour < 6;
+export const isNight = () => nowHour() >= 0 && nowHour() < 6;
 
-export const isMorning = () => nowHour > 5 && nowHour < 12;
+export const isMorning = () => nowHour() > 5 && nowHour() < 12;
 
-export const isAfternoon = () => nowHour > 11 && nowHour < 18;
+export const isAfternoon = () => nowHour() > 11 && nowHour() < 18;
 
-export const isEvening = () => nowHour > 17 && nowHour <= 23;
+export const isEvening = () => nowHour() > 17 && nowHour() <= 23;
 
-export const isBusinessHours = () => nowHour > 8 && nowHour < 17;
+export const isBusinessHours = () => nowHour() > 8 && nowHour() < 17;
 
-export const isDayShift = () => nowHour > 7 && nowHour < 16;
+export const isDayShift = () => nowHour() > 7 && nowHour() < 16;
 
-export const isEveningShift = () => nowHour > 15 && nowHour <= 23;
+export const isEveningShift = () => nowHour() > 15 && nowHour() <= 23;
 
-export const isNightShift = () => nowHour >= 0 && nowHour < 8;
+export const isNightShift = () => nowHour() >= 0 && nowHour() < 8;
