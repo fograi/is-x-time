a8e50bd76d0943d0cfb244f1c042fd8c860d8956
martinkilmartin <martinkilmartin@gmail.com>
2022-03-08 20:47:16 -0500

    Adds more time periods

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,19 @@ export const nowHour = nowDate.getHours();
 export const isDayTime = () => nowHour > 5 && nowHour < 18;
 
 export const isNightTime = () => !isDayTime();
+
+export const isNight = () => nowHour >= 0 && nowHour < 6;
+
+export const isMorning = () => nowHour > 5 && nowHour < 12;
+
+export const isAfternoon = () => nowHour > 11 && nowHour < 18;
+
+export const isEvening = () => nowHour > 17 && nowHour <= 23;
+
+export const isBusinessHours = () => nowHour > 8 && nowHour < 17;
+
+export const isDayShift = () => nowHour > 7 && nowHour < 16;
+
+export const isEveningShift = () => nowHour > 15 && nowHour <= 23;
+
+export const isNightShift = () => nowHour >= 0 && nowHour < 8;
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,12 +1,44 @@
-import { nowHour, isDayTime, isNightTime } from "../src/index";
+import * as MyTimes from "../src/index";
 
 test("now hour", () => {
-  expect(nowHour).toBeGreaterThanOrEqual(0);
-  expect(nowHour).toBeLessThanOrEqual(23);
+  expect(MyTimes.nowHour).toBeGreaterThanOrEqual(0);
+  expect(MyTimes.nowHour).toBeLessThanOrEqual(23);
 });
 test("day time", () => {
-  expect(isDayTime()).toEqual(nowHour > 5 && nowHour < 18 ? true : false);
+  expect(MyTimes.isDayTime()).toEqual(MyTimes.nowHour > 5 && MyTimes.nowHour < 18 ? true : false);
 });
 test("night time", () => {
-  expect(isNightTime()).toEqual(nowHour < 6 || nowHour > 17 ? true : false);
+  expect(MyTimes.isNightTime()).toEqual(MyTimes.nowHour < 6 || MyTimes.nowHour > 17 ? true : false);
+});
+
+test("night", () => {
+  expect(MyTimes.isNight()).toEqual(MyTimes.nowHour >= 0 && MyTimes.nowHour < 6 ? true : false);
+});
+
+test("morning", () => {
+  expect(MyTimes.isMorning()).toEqual(MyTimes.nowHour > 5 && MyTimes.nowHour < 12 ? true : false);
+});
+
+test("afternoon", () => {
+  expect(MyTimes.isAfternoon()).toEqual(MyTimes.nowHour > 5 && MyTimes.nowHour < 12 ? true : false);
+});
+
+test("evening", () => {
+  expect(MyTimes.isEvening()).toEqual(MyTimes.nowHour > 17 && MyTimes.nowHour <= 23 ? true : false);
+});
+
+test("business hours", () => {
+  expect(MyTimes.isBusinessHours()).toEqual(MyTimes.nowHour > 7 && MyTimes.nowHour < 17 ? true : false);
+});
+
+test("day shift", () => {
+  expect(MyTimes.isDayShift()).toEqual(MyTimes.nowHour > 7 && MyTimes.nowHour < 16 ? true : false);
+});
+
+test("evening shift", () => {
+  expect(MyTimes.isEveningShift()).toEqual(MyTimes.nowHour > 15 && MyTimes.nowHour <= 23 ? true : false);
+});
+
+test("night shift", () => {
+  expect(MyTimes.isNightShift()).toEqual(MyTimes.nowHour >= 0 && MyTimes.nowHour < 8 ? true : false);
 });
