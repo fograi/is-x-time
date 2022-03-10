353505a83dc2faeee03605339bf7997d67f2b5a5
fograi <dev@fogr.ai>
2022-03-09 20:23:29 -0500

    Adds fake timers for all tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,22 +1,34 @@
 import * as MyTimes from "../src/index";
 
 beforeEach(() => {
+  expect(new Date(Date.now()).getFullYear()).toBeGreaterThanOrEqual(2022);
   jest.useFakeTimers();
 });
 
 afterEach(() => {
   jest.runOnlyPendingTimers();
   jest.useRealTimers();
+  expect(new Date(Date.now()).getFullYear()).toBeGreaterThanOrEqual(2022);
 });
 
 test("now hour", () => {
+  jest.setSystemTime(new Date("2001-01-01T01:01:01"));
+  expect(MyTimes.nowHour()).toEqual(1);
   jest.setSystemTime(new Date("2011-11-11T11:11:11"));
   expect(MyTimes.nowHour()).toEqual(11);
+  jest.setSystemTime(new Date("2012-12-12T12:12:12"));
+  expect(MyTimes.nowHour()).toEqual(12);
   jest.setSystemTime(new Date("2022-02-22T22:22:22"));
   expect(MyTimes.nowHour()).toEqual(22);
 });
 test("day time", () => {
+  jest.setSystemTime(new Date("2001-01-01T01:01:01"));
+  expect(MyTimes.isDayTime()).toEqual(false);
+  expect(MyTimes.isDayTime()).toEqual(!MyTimes.isNightTime());
   jest.setSystemTime(new Date("2011-11-11T11:11:11"));
+  expect(MyTimes.isDayTime()).toEqual(true);
+  expect(MyTimes.isDayTime()).toEqual(!MyTimes.isNightTime());
+  jest.setSystemTime(new Date("2012-12-12T12:12:12"));
   expect(MyTimes.isDayTime()).toEqual(true);
   expect(MyTimes.isDayTime()).toEqual(!MyTimes.isNightTime());
   jest.setSystemTime(new Date("2022-02-22T22:22:22"));
@@ -24,7 +36,13 @@ test("day time", () => {
   expect(MyTimes.isDayTime()).toEqual(!MyTimes.isNightTime());
 });
 test("night time", () => {
+  jest.setSystemTime(new Date("2001-01-01T01:01:01"));
+  expect(MyTimes.isNightTime()).toEqual(true);
+  expect(MyTimes.isNightTime()).toEqual(!MyTimes.isDayTime());
   jest.setSystemTime(new Date("2011-11-11T11:11:11"));
+  expect(MyTimes.isNightTime()).toEqual(false);
+  expect(MyTimes.isNightTime()).toEqual(!MyTimes.isDayTime());
+  jest.setSystemTime(new Date("2012-12-12T12:12:12"));
   expect(MyTimes.isNightTime()).toEqual(false);
   expect(MyTimes.isNightTime()).toEqual(!MyTimes.isDayTime());
   jest.setSystemTime(new Date("2022-02-22T22:22:22"));
@@ -33,70 +51,173 @@ test("night time", () => {
 });
 
 test("night", () => {
+  jest.setSystemTime(new Date("2001-01-01T01:01:01"));
+  expect(MyTimes.isNight()).toEqual(true);
   expect(MyTimes.isNight()).toEqual(
-    MyTimes.nowHour() >= 0 && MyTimes.nowHour() < 6 ? true : false
+    !MyTimes.isMorning() && !MyTimes.isAfternoon() && !MyTimes.isEvening()
   );
+  jest.setSystemTime(new Date("2011-11-11T11:11:11"));
+  expect(MyTimes.isNight()).toEqual(false);
+  expect(MyTimes.isNight()).toEqual(
+    !MyTimes.isMorning() && !MyTimes.isAfternoon() && !MyTimes.isEvening()
+  );
+  jest.setSystemTime(new Date("2012-12-12T12:12:12"));
+  expect(MyTimes.isNight()).toEqual(false);
+  expect(MyTimes.isNight()).toEqual(
+    !MyTimes.isMorning() && !MyTimes.isAfternoon() && !MyTimes.isEvening()
+  );
+  jest.setSystemTime(new Date("2022-02-22T22:22:22"));
+  expect(MyTimes.isNight()).toEqual(false);
   expect(MyTimes.isNight()).toEqual(
     !MyTimes.isMorning() && !MyTimes.isAfternoon() && !MyTimes.isEvening()
   );
 });
 
 test("morning", () => {
+  jest.setSystemTime(new Date("2001-01-01T01:01:01"));
+  expect(MyTimes.isMorning()).toEqual(false);
   expect(MyTimes.isMorning()).toEqual(
-    MyTimes.nowHour() > 5 && MyTimes.nowHour() < 12 ? true : false
+    !MyTimes.isNight() && !MyTimes.isAfternoon() && !MyTimes.isEvening()
   );
+  jest.setSystemTime(new Date("2011-11-11T11:11:11"));
+  expect(MyTimes.isMorning()).toEqual(true);
+  expect(MyTimes.isMorning()).toEqual(
+    !MyTimes.isNight() && !MyTimes.isAfternoon() && !MyTimes.isEvening()
+  );
+  jest.setSystemTime(new Date("2012-12-12T12:12:12"));
+  expect(MyTimes.isMorning()).toEqual(false);
+  expect(MyTimes.isMorning()).toEqual(
+    !MyTimes.isNight() && !MyTimes.isAfternoon() && !MyTimes.isEvening()
+  );
+  jest.setSystemTime(new Date("2022-02-22T22:22:22"));
+  expect(MyTimes.isMorning()).toEqual(false);
   expect(MyTimes.isMorning()).toEqual(
     !MyTimes.isNight() && !MyTimes.isAfternoon() && !MyTimes.isEvening()
   );
 });
 
 test("afternoon", () => {
+  jest.setSystemTime(new Date("2001-01-01T01:01:01"));
+  expect(MyTimes.isAfternoon()).toEqual(false);
   expect(MyTimes.isAfternoon()).toEqual(
-    MyTimes.nowHour() > 11 && MyTimes.nowHour() < 18 ? true : false
+    !MyTimes.isNight() && !MyTimes.isMorning() && !MyTimes.isEvening()
   );
+  jest.setSystemTime(new Date("2011-11-11T11:11:11"));
+  expect(MyTimes.isAfternoon()).toEqual(false);
+  expect(MyTimes.isAfternoon()).toEqual(
+    !MyTimes.isNight() && !MyTimes.isMorning() && !MyTimes.isEvening()
+  );
+  jest.setSystemTime(new Date("2012-12-12T12:12:12"));
+  expect(MyTimes.isAfternoon()).toEqual(true);
+  expect(MyTimes.isAfternoon()).toEqual(
+    !MyTimes.isNight() && !MyTimes.isMorning() && !MyTimes.isEvening()
+  );
+  jest.setSystemTime(new Date("2022-02-22T22:22:22"));
+  expect(MyTimes.isAfternoon()).toEqual(false);
   expect(MyTimes.isAfternoon()).toEqual(
     !MyTimes.isNight() && !MyTimes.isMorning() && !MyTimes.isEvening()
   );
 });
 
 test("evening", () => {
-  expect(MyTimes.isEvening()).toEqual(
-    MyTimes.nowHour() > 17 && MyTimes.nowHour() <= 23 ? true : false
-  );
+  jest.setSystemTime(new Date("2001-01-01T01:01:01"));
+  expect(MyTimes.isEvening()).toEqual(false);
   expect(MyTimes.isEvening()).toEqual(
     !MyTimes.isNight() && !MyTimes.isMorning() && !MyTimes.isAfternoon()
+  );
+  jest.setSystemTime(new Date("2011-11-11T11:11:11"));
+  expect(MyTimes.isEvening()).toEqual(false);
+  expect(MyTimes.isEvening()).toEqual(
+    !MyTimes.isNight() && !MyTimes.isMorning() && !MyTimes.isAfternoon()
+  );
+  jest.setSystemTime(new Date("2012-12-12T12:12:12"));
+  expect(MyTimes.isEvening()).toEqual(false);
+  expect(MyTimes.isEvening()).toEqual(
+    !MyTimes.isNight() && !MyTimes.isMorning() && !MyTimes.isAfternoon()
+  );
+  jest.setSystemTime(new Date("2022-02-22T22:22:22"));
+  expect(MyTimes.isAfternoon()).toEqual(false);
+  expect(MyTimes.isAfternoon()).toEqual(
+    !MyTimes.isNight() && !MyTimes.isMorning() && !MyTimes.isEvening()
   );
 });
 
 test("business hours", () => {
-  expect(MyTimes.isBusinessHours()).toEqual(
-    MyTimes.nowHour() > 7 && MyTimes.nowHour() < 17 ? true : false
-  );
+  jest.setSystemTime(new Date("2001-01-01T01:01:01"));
+  expect(MyTimes.isBusinessHours()).toEqual(false);
+  jest.setSystemTime(new Date("2011-11-11T11:11:11"));
+  expect(MyTimes.isBusinessHours()).toEqual(true);
+  jest.setSystemTime(new Date("2012-12-12T12:12:12"));
+  expect(MyTimes.isBusinessHours()).toEqual(true);
+  jest.setSystemTime(new Date("2022-02-22T22:22:22"));
+  expect(MyTimes.isBusinessHours()).toEqual(false);
 });
 
 test("day shift", () => {
+  jest.setSystemTime(new Date("2001-01-01T01:01:01"));
+  expect(MyTimes.isDayShift()).toEqual(false);
   expect(MyTimes.isDayShift()).toEqual(
-    MyTimes.nowHour() > 7 && MyTimes.nowHour() < 16 ? true : false
+    !MyTimes.isEveningShift() && !MyTimes.isNightShift()
   );
+  jest.setSystemTime(new Date("2011-11-11T11:11:11"));
+  expect(MyTimes.isDayShift()).toEqual(true);
+  expect(MyTimes.isDayShift()).toEqual(
+    !MyTimes.isEveningShift() && !MyTimes.isNightShift()
+  );
+  jest.setSystemTime(new Date("2012-12-12T12:12:12"));
+  expect(MyTimes.isDayShift()).toEqual(true);
+  expect(MyTimes.isDayShift()).toEqual(
+    !MyTimes.isEveningShift() && !MyTimes.isNightShift()
+  );
+  jest.setSystemTime(new Date("2022-02-22T22:22:22"));
+  expect(MyTimes.isDayShift()).toEqual(false);
   expect(MyTimes.isDayShift()).toEqual(
     !MyTimes.isEveningShift() && !MyTimes.isNightShift()
   );
 });
 
 test("evening shift", () => {
+  jest.setSystemTime(new Date("2001-01-01T01:01:01"));
+  expect(MyTimes.isEveningShift()).toEqual(false);
   expect(MyTimes.isEveningShift()).toEqual(
-    MyTimes.nowHour() > 15 && MyTimes.nowHour() <= 23 ? true : false
+    !MyTimes.isDayShift() && !MyTimes.isNightShift()
   );
+  jest.setSystemTime(new Date("2011-11-11T11:11:11"));
+  expect(MyTimes.isEveningShift()).toEqual(false);
+  expect(MyTimes.isEveningShift()).toEqual(
+    !MyTimes.isDayShift() && !MyTimes.isNightShift()
+  );
+  jest.setSystemTime(new Date("2012-12-12T12:12:12"));
+  expect(MyTimes.isEveningShift()).toEqual(false);
+  expect(MyTimes.isEveningShift()).toEqual(
+    !MyTimes.isDayShift() && !MyTimes.isNightShift()
+  );
+  jest.setSystemTime(new Date("2022-02-22T22:22:22"));
+  expect(MyTimes.isEveningShift()).toEqual(true);
   expect(MyTimes.isEveningShift()).toEqual(
     !MyTimes.isDayShift() && !MyTimes.isNightShift()
   );
 });
 
 test("night shift", () => {
-  expect(MyTimes.isNightShift()).toEqual(
-    MyTimes.nowHour() >= 0 && MyTimes.nowHour() < 8 ? true : false
-  );
+  jest.setSystemTime(new Date("2001-01-01T01:01:01"));
+  expect(MyTimes.isNightShift()).toEqual(true);
   expect(MyTimes.isNightShift()).toEqual(
     !MyTimes.isDayShift() && !MyTimes.isEveningShift()
+  );
+  jest.setSystemTime(new Date("2011-11-11T11:11:11"));
+  expect(MyTimes.isNightShift()).toEqual(false);
+  expect(MyTimes.isNightShift()).toEqual(
+    !MyTimes.isDayShift() && !MyTimes.isEveningShift()
+  );
+  jest.setSystemTime(new Date("2012-12-12T12:12:12"));
+  expect(MyTimes.isNightShift()).toEqual(false);
+  expect(MyTimes.isNightShift()).toEqual(
+    !MyTimes.isDayShift() && !MyTimes.isEveningShift()
+  );
+  jest.setSystemTime(new Date("2022-02-22T22:22:22"));
+  expect(MyTimes.isEveningShift()).toEqual(true);
+  expect(MyTimes.isEveningShift()).toEqual(
+    !MyTimes.isDayShift() && !MyTimes.isNightShift()
   );
 });
