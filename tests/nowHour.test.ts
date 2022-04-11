d28dab8d2d45b3cbf2b5b1f0d891c293ae50511a
sionnach <sionnach.solutions@gmail.com>
2022-04-11 16:19:23 -0400

    Splits nowHours test

--- tests/nowHour.test.ts
+++ tests/nowHour.test.ts
@@ -12,57 +12,132 @@ afterEach(() => {
   expect(new Date(Date.now()).getFullYear()).toBeGreaterThanOrEqual(2022);
 });
 
-test("now hour", () => {
+test("now hour 1999_12_31_23_59", () => {
   jest.setSystemTime(new Date(DATES.DATE_1999_12_31_23_59));
   expect(nowHour()).toEqual(23);
+});
+
+test("now hour 2000_01_01_00_00", () => {
   jest.setSystemTime(new Date(DATES.DATE_2000_01_01_00_00));
   expect(nowHour()).toEqual(0);
+});
+
+test("now hour 2001_01_01_01_02", () => {
   jest.setSystemTime(new Date(DATES.DATE_2001_01_01_01_02));
   expect(nowHour()).toEqual(1);
+});
+
+test("now hour 2002_02_02_02_04", () => {
   jest.setSystemTime(new Date(DATES.DATE_2002_02_02_02_04));
   expect(nowHour()).toEqual(2);
+});
+
+test("now hour 2003_03_03_03_06", () => {
   jest.setSystemTime(new Date(DATES.DATE_2003_03_03_03_06));
   expect(nowHour()).toEqual(3);
+});
+
+test("now hour 2004_04_04_04_08", () => {
   jest.setSystemTime(new Date(DATES.DATE_2004_04_04_04_08));
   expect(nowHour()).toEqual(4);
+});
+
+test("now hour 2005_05_05_05_10", () => {
   jest.setSystemTime(new Date(DATES.DATE_2005_05_05_05_10));
   expect(nowHour()).toEqual(5);
+});
+
+test("now hour 2006_06_06_06_12", () => {
   jest.setSystemTime(new Date(DATES.DATE_2006_06_06_06_12));
   expect(nowHour()).toEqual(6);
+});
+
+test("now hour 2007_07_07_07_14", () => {
   jest.setSystemTime(new Date(DATES.DATE_2007_07_07_07_14));
   expect(nowHour()).toEqual(7);
+});
+
+test("now hour 2008_08_08_08_16", () => {
   jest.setSystemTime(new Date(DATES.DATE_2008_08_08_08_16));
   expect(nowHour()).toEqual(8);
+});
+
+test("now hour 2009_09_09_09_18", () => {
   jest.setSystemTime(new Date(DATES.DATE_2009_09_09_09_18));
   expect(nowHour()).toEqual(9);
+});
+
+test("now hour 2010_10_10_10_20", () => {
   jest.setSystemTime(new Date(DATES.DATE_2010_10_10_10_20));
   expect(nowHour()).toEqual(10);
+});
+
+test("now hour 2011_11_11_11_22", () => {
   jest.setSystemTime(new Date(DATES.DATE_2011_11_11_11_22));
   expect(nowHour()).toEqual(11);
+});
+
+test("now hour 2012_12_12_12_24", () => {
   jest.setSystemTime(new Date(DATES.DATE_2012_12_12_12_24));
   expect(nowHour()).toEqual(12);
+});
+
+test("now hour 2013_03_13_13_26", () => {
   jest.setSystemTime(new Date(DATES.DATE_2013_03_13_13_26));
   expect(nowHour()).toEqual(13);
+});
+
+test("now hour 2014_04_14_14_28", () => {
   jest.setSystemTime(new Date(DATES.DATE_2014_04_14_14_28));
   expect(nowHour()).toEqual(14);
+});
+
+test("now hour 2015_05_15_15_30", () => {
   jest.setSystemTime(new Date(DATES.DATE_2015_05_15_15_30));
   expect(nowHour()).toEqual(15);
+});
+
+test("now hour 2016_06_16_16_32", () => {
   jest.setSystemTime(new Date(DATES.DATE_2016_06_16_16_32));
   expect(nowHour()).toEqual(16);
+});
+
+test("now hour 2017_07_17_17_34", () => {
   jest.setSystemTime(new Date(DATES.DATE_2017_07_17_17_34));
   expect(nowHour()).toEqual(17);
+});
+
+test("now hour 2018_08_18_18_36", () => {
   jest.setSystemTime(new Date(DATES.DATE_2018_08_18_18_36));
   expect(nowHour()).toEqual(18);
+});
+
+test("now hour 2019_09_19_19_38", () => {
   jest.setSystemTime(new Date(DATES.DATE_2019_09_19_19_38));
   expect(nowHour()).toEqual(19);
+});
+
+test("now hour ", () => {
   jest.setSystemTime(new Date(DATES.DATE_2020_02_20_20_40));
   expect(nowHour()).toEqual(20);
+});
+
+test("now hour 2021_01_21_21_42", () => {
   jest.setSystemTime(new Date(DATES.DATE_2021_01_21_21_42));
   expect(nowHour()).toEqual(21);
+});
+
+test("now hour 2022_02_22_22_44", () => {
   jest.setSystemTime(new Date(DATES.DATE_2022_02_22_22_44));
   expect(nowHour()).toEqual(22);
+});
+
+test("now hour 2023_03_23_23_46", () => {
   jest.setSystemTime(new Date(DATES.DATE_2023_03_23_23_46));
   expect(nowHour()).toEqual(23);
+});
+
+test("now hour 2024_04_24_00_48", () => {
   jest.setSystemTime(new Date(DATES.DATE_2024_04_24_00_48));
   expect(nowHour()).toEqual(0);
 });
